8fb98a3160c87f9ede1921d0ecd8ecabb8ec694f
Al3x Ivanov <pro87@yandex.ru>
2025-03-09 16:22:15 +0400

    Removed unused import

--- src/main/java/ru/yandex/practicum/filmorate/dao/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/FilmDbStorage.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.dao;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Primary;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
